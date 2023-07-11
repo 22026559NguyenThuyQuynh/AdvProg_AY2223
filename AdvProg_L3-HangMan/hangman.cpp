af5d2f51541e9de3fc982cf96f66ed2705daca40
22026559NguyenThuyQuynh <22026559@vnu.edu.vn>
2023-07-11 10:56:38 +0700

    update hang man

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -167,7 +167,7 @@ void processData(const char ch, const string& word,
    }
    else{
         updateIncorrectGuess(incorrectGuess);
-        updateEnteredChars(ch, correctChars);
+        updateEnteredChars(ch, incorrectChars);
    }
 }
 
